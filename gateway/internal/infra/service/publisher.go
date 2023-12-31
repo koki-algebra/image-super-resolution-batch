b2964009deb240ce0b5ce4f4ea828e7aec78dddc
koki-algebra <yotsuba00040@gmail.com>
2023-12-31 17:41:42 +0900

    fix publish

--- gateway/internal/infra/service/publisher.go
+++ gateway/internal/infra/service/publisher.go
@@ -2,16 +2,76 @@ package service
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/koki-algebra/image-super-resolution-batch/gateway/internal/config"
 	"github.com/koki-algebra/image-super-resolution-batch/gateway/internal/service"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func NewPublisher() service.Publisher {
-	return &publisherImpl{}
+func NewPublisher(cfg *config.Config) (service.Publisher, error) {
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d", cfg.MQUser, cfg.MQPassword, cfg.MQHost, cfg.MQPort))
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		cfg.MQQueueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &publisherImpl{
+		cfg: cfg,
+		q:   q,
+	}, nil
 }
 
-type publisherImpl struct{}
+type publisherImpl struct {
+	cfg *config.Config
+	q   amqp.Queue
+}
 
-func (p *publisherImpl) PushObject(ctx context.Context, key string) error {
+func (p *publisherImpl) Publish(ctx context.Context, contentType string, body []byte) error {
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d", p.cfg.MQUser, p.cfg.MQPassword, p.cfg.MQHost, p.cfg.MQPort))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	if err := ch.PublishWithContext(
+		ctx,
+		"",
+		p.q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  contentType,
+			Body:         body,
+		},
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
--- gateway/internal/usecase/image.go
+++ gateway/internal/usecase/image.go
@@ -32,14 +32,14 @@ type imageImpl struct {
 	repo      repository.History
 }
 
-func (i *imageImpl) Publish(ctx context.Context, r io.Reader, extension string) (*entity.History, error) {
+func (img *imageImpl) Publish(ctx context.Context, r io.Reader, extension string) (*entity.History, error) {
 	jobID := uuid.NewString()
 	history := entity.History{
 		Status: entity.PENDING,
 		IsrJob: entity.IsrJob{
 			IsrJobID:                jobID,
-			UploadImageKey:          fmt.Sprintf("%s/%s%s", i.cfg.UploadImagePrefix, jobID, extension),
-			SuperResolutionImageKey: fmt.Sprintf("%s/%s%s", i.cfg.SuperResolutionImagePrefix, jobID, extension),
+			UploadImageKey:          fmt.Sprintf("%s/%s%s", img.cfg.UploadImagePrefix, jobID, extension),
+			SuperResolutionImageKey: fmt.Sprintf("%s/%s%s", img.cfg.SuperResolutionImagePrefix, jobID, extension),
 		},
 	}
 
@@ -47,19 +47,19 @@ func (i *imageImpl) Publish(ctx context.Context, r io.Reader, extension string) 
 	imageKey := history.IsrJob.UploadImageKey
 
 	// Create history
-	if err := i.repo.Create(ctx, &history); err != nil {
+	if err := img.repo.Create(ctx, &history); err != nil {
 		return nil, err
 	}
 
 	// Upload the uploaded image to object storage
-	if err := i.storage.PutObject(ctx, imageKey, r); err != nil {
+	if err := img.storage.PutObject(ctx, imageKey, r); err != nil {
 		return nil, err
 	}
 
 	// Push image key to messaging queue
-	if err := i.publisher.PushObject(ctx, imageKey); err != nil {
+	if err := img.publisher.Publish(ctx, "text/plain", []byte(jobID)); err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	return &history, nil
 }
