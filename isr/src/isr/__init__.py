00f862091d58fde1576ceccb84f4b0b5769284b5
koki-algebra <yotsuba00040@gmail.com>
2024-01-01 16:38:50 +0000

    add ORM

--- isr/src/isr/__init__.py
+++ isr/src/isr/__init__.py
@@ -1,2 +1,13 @@
+import logging
+
+from isr.worker import Worker
+
+
 def main():
-    print("Hello, World!")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname)-9s %(asctime)s [%(filename)s:%(lineno)d] %(message)s",
+    )
+
+    worker = Worker()
+    worker.run()
